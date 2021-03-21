d006c2dabd87cc0fbab44fed311a8141f1d1df89
Andreas Rudolf <anrudolf@gmail.com>
2021-03-21 23:36:08 +0100

    Added border around list

--- src/components/KasseListe.vue
+++ src/components/KasseListe.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="flex">
-    <ul class="wrapper" :style="`grid-template-rows: repeat(${pageSize}, 1fr)`">
+  <div class="bg-gray-300 kasse-liste-wrapper">
+    <div class="p-2" style="grid-area: header">
+      <h1 class="text-4xl text-center">Ihr Einkauf</h1>
+    </div>
+    <ul
+      style="grid-area: list"
+      class="kasse-liste-list-wrapper ml-2 bg-white"
+      :style="`grid-template-rows: repeat(${pageSize}, 1fr)`"
+    >
       <li
         v-for="(item, i) in displayedItems"
         :key="item.code"
@@ -14,7 +21,10 @@
         />
       </li>
     </ul>
-    <div class="flex flex-col mx-3">
+    <div
+      style="grid-area: sidebar"
+      class="flex flex-col items-center mr-2 bg-white"
+    >
       <button
         class="my-1 rounded border-2 w-12 h-12 flex justify-center items-center bg-gradient-to-br from-gray-100 to-gray-200"
         @click="$store.dispatch('kasse/prev')"
@@ -32,6 +42,7 @@
         <app-icon icon="arrow-page-down" color="gray" />
       </button>
     </div>
+    <div style="grid-area: footer" />
   </div>
 </template>
 
@@ -98,11 +109,21 @@ li:nth-child(even) {
   background: #fff;
 }
 
+.kasse-liste-wrapper {
+  display: grid;
+  grid-template-rows: 3.5rem 1fr 1rem;
+  grid-template-columns: 1fr 5rem;
+  grid-gap: 0rem;
+  grid-template-areas:
+    "header header"
+    "list sidebar"
+    "footer footer";
+}
+
 /* TODO: dynamic on pageSize */
-.wrapper {
+.kasse-liste-list-wrapper {
   display: grid;
   grid-template-columns: 1fr;
-
   /*grid-template-rows: repeat(3, 1fr);*/
 }
 
