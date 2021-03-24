89f830157309a444ad5e7785fec000d54fd52b96
Andreas Rudolf <anrudolf@gmail.com>
2021-03-24 21:37:46 +0100

    Added firebase config, adapted tailwind

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export default {
+  apiKey: "AIzaSyDmtBO5WoQYH6LzRwGxDd4sIP9vYg3E5Wg",
+  authDomain: "kasseli.firebaseapp.com",
+  projectId: "kasseli",
+  storageBucket: "kasseli.appspot.com",
+  messagingSenderId: "476135930198",
+  appId: "1:476135930198:web:60ffaa7b9fbe860998fef7",
+};
