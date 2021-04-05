e75aa363ff8669dcbe31db04c94a33d9d71f6074
Andreas Rudolf <anrudolf@gmail.com>
2021-04-05 18:40:07 +0200

    Use bills in drop area

--- src/views/PayCash.vue
+++ src/views/PayCash.vue
@@ -1,45 +1,35 @@
 <template>
-  <app-money-bill :amount="20" />
+  <draggable
+    class="dragArea flex justify-center items-center relative h-48 w-48 border"
+    :list="list2"
+    group="people"
+    @change="log"
+    item-key="name"
+  >
+    <app-money-bill
+      v-for="(element, i) in list2"
+      :key="`${element.id}-${i}`"
+      class="absolute p-3 inline-block m-1 w-20"
+      :style="{ transform: `rotate(${3 * i}deg)` }"
+      :amount="element.id"
+    />
+  </draggable>
 
-  <div class="relative h-32 p-8">
-    <h3>Basket</h3>
-    <draggable
-      class="dragArea list-group"
-      :list="list2"
-      group="people"
-      @change="log"
-      item-key="name"
-    >
-      <div
-        v-for="(element, i) in list2"
-        :key="`${element.id}-${i}`"
-        class="absolute p-3 border inline-block m-1"
-        :style="{ transform: `rotate(${2 * i}deg)` }"
-      >
-        {{ element.name }}
-      </div>
-    </draggable>
-  </div>
-
-  <div class="">
-    <h3>Cash</h3>
-    <draggable
-      class="dragArea list-group"
-      :list="list1"
-      :group="{ name: 'people', pull: 'clone', put: false }"
-      @change="log"
-      item-key="name"
-    >
-      <div
-        v-for="element in list1"
-        :key="element.id"
-        class="cursor-pointer p-3 border inline-block m-1"
-        @click="add(element)"
-      >
-        {{ element.name }}
-      </div>
-    </draggable>
-  </div>
+  <draggable
+    class="dragArea flex items-start"
+    :list="list1"
+    :group="{ name: 'people', pull: 'clone', put: false }"
+    @change="log"
+    item-key="name"
+  >
+    <app-money-bill
+      v-for="element in list1"
+      :key="element.id"
+      class="cursor-pointer m-2 inline-block w-24"
+      :amount="element.id"
+      @click="add(element)"
+    />
+  </draggable>
 </template>
 
 <script>
@@ -58,16 +48,13 @@ export default {
   data() {
     return {
       list1: [
-        { name: "200 CHF", id: 200 },
-        { name: "100 CHF", id: 100 },
+        { name: "10 CHF", id: 10 },
+        { name: "20 CHF", id: 20 },
         { name: "50 CHF", id: 50 },
-        { name: "20 CHF", id: 20 },
-        { name: "10 CHF", id: 10 },
+        { name: "100 CHF", id: 100 },
+        { name: "200 CHF", id: 200 },
       ],
-      list2: [
-        { name: "20 CHF", id: 20 },
-        { name: "10 CHF", id: 10 },
-      ],
+      list2: [],
     };
   },
   methods: {
