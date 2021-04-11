a0711964a6f06f798e6a61746f8162d7803c3b6d
Andreas Rudolf <anrudolf@gmail.com>
2021-04-11 02:14:59 +0200

    Improve cash

--- src/views/PayCash.vue
+++ src/views/PayCash.vue
@@ -3,62 +3,124 @@
     >Zurück</app-button-back
   >
 
-  <draggable
-    class="my-4 dragArea flex justify-center items-center relative h-48 w-full border"
-    :list="counter"
-    group="cash"
-    @change="log"
-    item-key="name"
-  >
-    <app-money-note
-      v-for="(element, i) in counter"
-      :key="`${element.id}-${i}`"
-      class="absolute p-3 inline-block m-1 w-20"
-      :style="{
-        transform: `translate(${i * 5}px, 0px) rotate(${4 * (i - 4)}deg)`,
-      }"
-      :amount="element.id"
-    />
-  </draggable>
+  <div class="p-4">
+    <draggable
+      class="my-4 dragArea flex justify-center items-center relative h-48 w-full border-4 border-dashed"
+      :list="counter"
+      :group="{ name: 'cash' }"
+      :sort="false"
+      item-key="name"
+      @click="pop"
+    >
+      <div
+        class="absolute p-3 inline-block m-1 w-24"
+        v-for="(element, i) in counter.filter((e) => e.type === 'note')"
+        :key="`${element.id}-${i}`"
+      >
+        <app-money-note
+          :style="{
+            transform: `translate(${
+              20 * Math.sin((i / 20.0) * 2 * Math.PI - Math.PI / 2)
+            }px, 0px) rotate(${
+              20 * Math.sin((i / 20.0) * 2 * Math.PI - Math.PI / 2)
+            }deg)`,
+          }"
+          :amount="element.id"
+        />
+      </div>
+      <div
+        class="absolute p-3 inline-block m-1 w-20"
+        v-for="(element, i) in counter.filter((e) => e.type === 'coin')"
+        :key="`${element.id}-${i}`"
+      >
+        <app-money-coin
+          :style="{
+            transform: `translate(${Math.random() * 20 - 10}px, ${
+              Math.random() * 20 - 10
+            }px) rotate(${Math.random() * 40 - 20}deg)`,
+          }"
+          :amount="element.id"
+        />
+      </div>
+    </draggable>
+  </div>
 
-  <draggable
-    class="dragArea flex items-start justify-center"
-    :list="wallet"
-    :group="{ name: 'cash', pull: 'clone', put: false }"
-    @change="log"
-    item-key="name"
-  >
-    <app-money-note
-      v-for="element in wallet"
-      :key="element.id"
-      class="cursor-pointer m-2 inline-block w-24"
-      :amount="element.id"
-      @click="add(element)"
-    />
-  </draggable>
+  <div v-if="!isCashReady" class="flex justify-center">
+    <app-wallet @click="handleWallet" :isClosed="isWalletClosed" />
+  </div>
+  <div v-else>
+    <draggable
+      class="dragArea flex items-start justify-center"
+      :list="notes"
+      :group="{ name: 'cash', pull: 'clone', put: false }"
+      @change="log"
+      item-key="name"
+    >
+      <app-money-note
+        v-for="element in notes"
+        :key="element.id"
+        class="cursor-pointer m-2 inline-block w-24"
+        :amount="element.id"
+        @click="add(element)"
+      />
+    </draggable>
+
+    <draggable
+      class="dragArea flex items-start justify-center"
+      :list="coins"
+      :group="{ name: 'cash', pull: 'clone', put: false }"
+      @change="log"
+      item-key="name"
+    >
+      <app-money-coin
+        v-for="element in coins"
+        :key="element.id"
+        class="cursor-pointer m-2 inline-block w-24"
+        :amount="element.id"
+        @click="add(element)"
+      />
+    </draggable>
+  </div>
 </template>
 
 <script>
 import { VueDraggableNext } from "vue-draggable-next";
 
 import appMoneyNote from "@/components/MoneyNote.vue";
+import appMoneyCoin from "@/components/MoneyCoin.vue";
+
 import appButtonBack from "@/components/ButtonBack.vue";
+
+import appWallet from "@/components/Wallet.vue";
 
 export default {
   components: {
     draggable: VueDraggableNext,
     appMoneyNote,
+    appMoneyCoin,
     appButtonBack,
+    appWallet,
   },
   data() {
     return {
-      wallet: [
-        { name: "10 CHF", id: 10 },
-        { name: "20 CHF", id: 20 },
-        { name: "50 CHF", id: 50 },
-        { name: "100 CHF", id: 100 },
+      notes: [
+        { type: "note", name: "10 CHF", id: 10 },
+        { type: "note", name: "20 CHF", id: 20 },
+        { type: "note", name: "50 CHF", id: 50 },
+        { type: "note", name: "100 CHF", id: 100 },
+      ],
+      coins: [
+        { type: "coin", name: "0.05 CHF", id: 0.05 },
+        { type: "coin", name: "0.10 CHF", id: 0.1 },
+        { type: "coin", name: "0.20 CHF", id: 0.2 },
+        { type: "coin", name: "0.50 CHF", id: 0.5 },
+        { type: "coin", name: "1.00 CHF", id: 1.0 },
+        { type: "coin", name: "2.00 CHF", id: 2.0 },
+        { type: "coin", name: "5.00 CHF", id: 5.0 },
       ],
       counter: [],
+      isWalletClosed: true,
+      isCashReady: false,
     };
   },
   methods: {
@@ -68,7 +130,19 @@ export default {
     add(item) {
       this.counter.push({ ...item });
     },
+    pop() {
+      this.counter.pop();
+    },
+    handleWallet() {
+      if (this.isWalletClosed) {
+        this.isWalletClosed = false;
+        return;
+      }
+
+      this.isCashReady = true;
+    },
   },
 };
 </script>
-<style scoped></style>
+<style scoped>
+</style>
