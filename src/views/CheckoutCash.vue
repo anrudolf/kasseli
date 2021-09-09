bb31e373f2c8d5d7a3de1928bafad023ee420221
Andreas Rudolf <anrudolf@gmail.com>
2021-09-10 00:15:41 +0200

    Rework cash checkout

--- src/views/CheckoutCash.vue
+++ src/views/CheckoutCash.vue
@@ -3,146 +3,135 @@
     >Zurück</app-button-back
   >
 
-  <div class="p-4">
-    <draggable
-      class="my-4 dragArea flex justify-center items-center relative h-36 w-full border-4 border-dashed"
-      :list="counter"
-      :group="{ name: 'cash' }"
-      :sort="false"
-      item-key="name"
-      @click="pop"
-    >
-      <div
-        class="absolute p-3 inline-block m-1 w-24"
-        v-for="(element, i) in counter.filter((e) => e.type === 'note')"
-        :key="`${element.id}-${i}`"
-      >
-        <app-money-note
-          :style="{
-            transform: `translate(${
-              20 * Math.sin((i / 20.0) * 2 * Math.PI - Math.PI / 2)
-            }px, 0px) rotate(${
-              20 * Math.sin((i / 20.0) * 2 * Math.PI - Math.PI / 2)
-            }deg)`,
-          }"
-          :amount="element.id"
-        />
-      </div>
-      <div
-        class="absolute p-3 inline-block m-1 w-20"
-        v-for="(element, i) in counter.filter((e) => e.type === 'coin')"
-        :key="`${element.id}-${i}`"
-      >
-        <app-money-coin
-          :style="{
-            transform: `translate(${Math.random() * 20 - 10}px, ${
-              Math.random() * 20 - 10
-            }px) rotate(${Math.random() * 40 - 20}deg)`,
-          }"
-          :amount="element.id"
-        />
-      </div>
-    </draggable>
-  </div>
+  <div class="p-2 max-w-md">
+    <h1>Bezahlen</h1>
+    <h2>Bar</h2>
 
-  <div v-if="!isCashReady" class="flex justify-center">
-    <app-wallet @click="handleWallet" :isClosed="isWalletClosed" />
-  </div>
-  <div v-else>
-    <draggable
-      class="dragArea flex items-start justify-center"
-      :list="notes"
-      :group="{ name: 'cash', pull: 'clone', put: false }"
-      @change="log"
-      item-key="name"
+    <div
+      class="
+        h-20
+        border
+        p-2
+        mt-2
+        bg-blue-400
+        rounded
+        text-white text-2xl
+        flex
+        justify-between
+        items-center
+      "
     >
+      <span>Betrag</span>
+      <span>{{ remainder.toFixed(2) }} CHF</span>
+    </div>
+
+    <div class="small-coin-grid mt-2">
+      <div v-for="element in smallCoins" :key="element.id">
+        <app-money-coin
+          class="cursor-pointer inline-block min-w-full"
+          :amount="element.id"
+          @click="add(element.id)"
+        />
+      </div>
+    </div>
+
+    <div class="big-coin-grid">
+      <div v-for="element in bigCoins" :key="element.id">
+        <app-money-coin
+          class="cursor-pointer inline-block min-w-full"
+          :amount="element.id"
+          @click="add(element.id)"
+        />
+      </div>
+    </div>
+
+    <div class="mt-2">
       <app-money-note
         v-for="element in notes"
         :key="element.id"
         class="cursor-pointer m-2 inline-block w-16"
         :amount="element.id"
-        @click="add(element)"
+        @click="add(element.id)"
       />
-    </draggable>
-
-    <draggable
-      class="dragArea flex flex-wrap items-start justify-center"
-      :list="coins"
-      :group="{ name: 'cash', pull: 'clone', put: false }"
-      @change="log"
-      item-key="name"
-    >
-      <app-money-coin
-        v-for="element in coins"
-        :key="element.id"
-        class="cursor-pointer m-2 inline-block w-16"
-        :amount="element.id"
-        @click="add(element)"
-      />
-    </draggable>
+    </div>
   </div>
 </template>
 
-<script>
-import { VueDraggableNext } from "vue-draggable-next";
+<script lang="ts">
+import { defineComponent, ref, computed } from "vue";
 
 import appMoneyNote from "@/components/MoneyNote.vue";
 import appMoneyCoin from "@/components/MoneyCoin.vue";
 
 import appButtonBack from "@/components/ButtonBack.vue";
 
-import appWallet from "@/components/Wallet.vue";
+import useKasseStore from "@/store/kasse";
 
-export default {
+export default defineComponent({
   components: {
-    draggable: VueDraggableNext,
     appMoneyNote,
     appMoneyCoin,
     appButtonBack,
-    appWallet,
   },
-  data() {
-    return {
-      notes: [
-        { type: "note", name: "10 CHF", id: 10 },
-        { type: "note", name: "20 CHF", id: 20 },
-        { type: "note", name: "50 CHF", id: 50 },
-        { type: "note", name: "100 CHF", id: 100 },
-      ],
-      coins: [
-        { type: "coin", name: "0.05 CHF", id: 0.05 },
-        { type: "coin", name: "0.10 CHF", id: 0.1 },
-        { type: "coin", name: "0.20 CHF", id: 0.2 },
-        { type: "coin", name: "0.50 CHF", id: 0.5 },
-        { type: "coin", name: "1.00 CHF", id: 1.0 },
-        { type: "coin", name: "2.00 CHF", id: 2.0 },
-        { type: "coin", name: "5.00 CHF", id: 5.0 },
-      ],
-      counter: [],
-      isWalletClosed: true,
-      isCashReady: false,
-    };
-  },
-  methods: {
-    log: function (evt) {
-      window.console.log(evt);
-    },
-    add(item) {
-      this.counter.push({ ...item });
-    },
-    pop() {
-      this.counter.pop();
-    },
-    handleWallet() {
-      if (this.isWalletClosed) {
-        this.isWalletClosed = false;
-        return;
-      }
+  setup() {
+    const kasse = useKasseStore();
 
-      this.isCashReady = true;
-    },
+    const notes = [
+      { type: "note", name: "10 CHF", id: 10 },
+      { type: "note", name: "20 CHF", id: 20 },
+      { type: "note", name: "50 CHF", id: 50 },
+      { type: "note", name: "100 CHF", id: 100 },
+    ];
+
+    const coins = [
+      { type: "coin", name: "0.05 CHF", id: 0.05 },
+      { type: "coin", name: "0.10 CHF", id: 0.1 },
+      { type: "coin", name: "0.20 CHF", id: 0.2 },
+      { type: "coin", name: "0.50 CHF", id: 0.5 },
+      { type: "coin", name: "1.00 CHF", id: 1.0 },
+      { type: "coin", name: "2.00 CHF", id: 2.0 },
+      { type: "coin", name: "5.00 CHF", id: 5.0 },
+    ];
+
+    const smallCoins = [
+      { type: "coin", name: "0.05 CHF", id: 0.05 },
+      { type: "coin", name: "0.10 CHF", id: 0.1 },
+      { type: "coin", name: "0.20 CHF", id: 0.2 },
+      { type: "coin", name: "0.50 CHF", id: 0.5 },
+    ];
+
+    const bigCoins = [
+      { type: "coin", name: "1.00 CHF", id: 1.0 },
+      { type: "coin", name: "2.00 CHF", id: 2.0 },
+      { type: "coin", name: "5.00 CHF", id: 5.0 },
+    ];
+
+    const total = kasse.price;
+    const paid = ref(0);
+    const remainder = computed(() => {
+      return total - paid.value;
+    });
+
+    const add = (i: number) => {
+      paid.value += i;
+    };
+
+    return { notes, coins, smallCoins, bigCoins, total, paid, add, remainder };
   },
-};
+});
 </script>
 <style scoped>
+.small-coin-grid {
+  display: grid;
+  grid-template-columns: 0.5fr 0.7fr 0.8fr 0.6fr;
+  align-items: center;
+  gap: 25px;
+}
+
+.big-coin-grid {
+  display: grid;
+  grid-template-columns: 1fr 1.2fr 1.5fr;
+  align-items: center;
+  gap: 20px;
+}
 </style>
