94b75e96d5ddd76ae7f9f69ed2282699c2da8a9e
Andreas Rudolf <anrudolf@gmail.com>
2021-09-17 01:31:37 +0200

    Fix jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,24 @@
+const path = require("path");
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript',
+  rootDir: path.resolve(__dirname),
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  moduleFileExtensions: ["vue", "js", "json", "jsx", "ts", "tsx", "node"],
+  // The alias set
+  moduleNameMapper: {
+    "@/(.*)$": "<rootDir>/src/$1",
+  },
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  // The test file
+  testMatch: [
+    "<rootDir>/tests/unit/*.spec.ts?(x)",
+    "<rootDir>/tests/unit/*.spec.js",
+  ],
   transform: {
-    '^.+\\.vue$': 'vue-jest'
-  }
-}
+    "^.+\\.vue$": "vue-jest",
+    "^.+\\js$": "babel-jest",
+    "^.+\\.(t|j)sx?$": "ts-jest",
+  },
+};
