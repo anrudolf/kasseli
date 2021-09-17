bfeb330bed0f24aef4c48e93320598978cee04d3
Andreas Rudolf <anrudolf@gmail.com>
2021-09-17 17:55:16 +0200

    Use prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:vue/vue3-recommended",
     "@vue/typescript/recommended",
+    "prettier",
   ],
   parserOptions: {
     ecmaVersion: 2020,
