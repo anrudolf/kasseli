b385fb2b291854a3ec01717897b1a0f95643a09b
Andreas Rudolf <anrudolf@gmail.com>
2022-01-28 22:06:34 +0100

    Continue styling on profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,69 +1,121 @@
 <template>
-  <app-modal v-model="showUpdatePasswordModal" title="Paswort ändern">
-    <div class="flex flex-col space-y-3">
-      <input
-        v-model="currentPassword"
-        type="password"
-        placeholder="Altes Passwort"
-        class="input"
-      />
-      <input
-        v-model="newPassword"
-        type="password"
-        placeholder="Neues Passwort"
-        class="input"
-      />
-      <input
-        v-model="newPasswordConfirmation"
-        type="password"
-        placeholder="Neues Passwort (Wiederholung)"
-        class="input"
-      />
-      <button class="btn btn-blue ali" @click="initiatePasswordUpdate">
-        Passwort ändern
-      </button>
-      <app-error-box v-model="errMsg" class="my-3"></app-error-box>
-      <app-success-box v-model="successMsg" class="my-3"></app-success-box>
-    </div>
-  </app-modal>
-
-  <app-modal v-model="showUpdateEmailModal" title="Email ändern">
-    <div class="flex flex-col space-y-3">
-      <input
-        v-model="currentPassword"
-        type="password"
-        placeholder="Passwort"
-        class="input"
-      />
-      <input v-model="newEmail" placeholder="Neue Email" class="input" />
-      <input
-        v-model="newEmailConfirmation"
-        placeholder="Neues Email (Wiederholung)"
-        class="input"
-      />
-      <button class="btn btn-blue ali" @click="initiateEmailUpdate">
-        Email ändern
-      </button>
-      <app-error-box v-model="errMsg" class="my-3"></app-error-box>
-      <app-success-box v-model="successMsg" class="my-3"></app-success-box>
-    </div>
-  </app-modal>
-
   <div class="p-4 max-w-md">
     <h1>Profile</h1>
     <div v-if="store.user != null">
       Signed in
       <div>{{ store.user.uid }} -- {{ store.user.email }}</div>
     </div>
-    <div class="flex flex-col space-y-3">
-      <button class="btn btn-blue" @click="signOut">Sign out</button>
-      <button class="btn btn-blue" @click="showUpdatePasswordModal = true">
-        Passwort ändern
-      </button>
-      <button class="btn btn-blue" @click="showUpdateEmailModal = true">
-        Email ändern
-      </button>
-    </div>
+  </div>
+  <div class="max-w-md p-4">
+    <Disclosure class="my-2" as="div">
+      <DisclosureButton
+        class="app-disclosure-button"
+        @click="showSignout = !showSignout"
+      >
+        <span>Ausloggen...</span>
+        <chevron-up-icon
+          :class="showSignout ? '' : 'transform rotate-180'"
+          class="w-5 h-5 text-blue-500"
+        />
+      </DisclosureButton>
+      <div v-show="showSignout">
+        <DisclosurePanel class="app-disclosure-panel" static>
+          <div v-if="store.user" class="flex flex-col space-y-3">
+            Du bist eingeloggt als {{ store.user.email }}
+            <button class="btn btn-blue ali" @click="initiateSignOut">
+              Logout
+            </button>
+            <app-error-box v-model="errMsg" class="my-3"></app-error-box>
+            <app-success-box
+              v-model="successMsg"
+              class="my-3"
+            ></app-success-box>
+          </div>
+        </DisclosurePanel>
+      </div>
+    </Disclosure>
+    <Disclosure class="my-2" as="div">
+      <DisclosureButton
+        class="app-disclosure-button"
+        @click="showUpdatePassword = !showUpdatePassword"
+      >
+        <span>Passwort ändern...</span>
+        <chevron-up-icon
+          :class="showUpdatePassword ? '' : 'transform rotate-180'"
+          class="w-5 h-5 text-blue-500"
+        />
+      </DisclosureButton>
+      <div v-show="showUpdatePassword">
+        <DisclosurePanel class="app-disclosure-panel" static>
+          <div class="flex flex-col space-y-3">
+            <input
+              v-model="currentPassword"
+              type="password"
+              placeholder="Altes Passwort"
+              class="input"
+            />
+            <input
+              v-model="newPassword"
+              type="password"
+              placeholder="Neues Passwort"
+              class="input"
+            />
+            <input
+              v-model="newPasswordConfirmation"
+              type="password"
+              placeholder="Neues Passwort (Wiederholung)"
+              class="input"
+            />
+            <button class="btn btn-blue ali" @click="initiatePasswordUpdate">
+              Passwort ändern
+            </button>
+            <app-error-box v-model="errMsg" class="my-3"></app-error-box>
+            <app-success-box
+              v-model="successMsg"
+              class="my-3"
+            ></app-success-box>
+          </div>
+        </DisclosurePanel>
+      </div>
+    </Disclosure>
+    <Disclosure class="my-2" as="div">
+      <DisclosureButton
+        class="app-disclosure-button"
+        @click="showUpdateEmail = !showUpdateEmail"
+      >
+        <span>Email ändern...</span>
+        <chevron-up-icon
+          :class="showUpdateEmail ? '' : 'transform rotate-180'"
+          class="w-5 h-5 text-blue-500"
+        />
+      </DisclosureButton>
+      <div v-show="showUpdateEmail">
+        <DisclosurePanel class="app-disclosure-panel" static>
+          <div class="flex flex-col space-y-3">
+            <input
+              v-model="currentPassword"
+              type="password"
+              placeholder="Passwort"
+              class="input"
+            />
+            <input v-model="newEmail" placeholder="Neue Email" class="input" />
+            <input
+              v-model="newEmailConfirmation"
+              placeholder="Neues Email (Wiederholung)"
+              class="input"
+            />
+            <button class="btn btn-blue ali" @click="initiateEmailUpdate">
+              Email ändern
+            </button>
+            <app-error-box v-model="errMsg" class="my-3"></app-error-box>
+            <app-success-box
+              v-model="successMsg"
+              class="my-3"
+            ></app-success-box>
+          </div>
+        </DisclosurePanel>
+      </div>
+    </Disclosure>
   </div>
 </template>
 
@@ -76,9 +128,12 @@ import {
   EmailAuthProvider,
   updatePassword,
   updateEmail,
+  signOut,
 } from "firebase/auth";
 
-import appModal from "@/components/ui/Modal.vue";
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+import { ChevronUpIcon } from "@heroicons/vue/solid";
+
 import appSuccessBox from "@/components/ui/SuccessBox.vue";
 import appErrorBox from "@/components/ui/ErrorBox.vue";
 
@@ -93,13 +148,15 @@ const store = useAuthStore();
 const errMsg = ref("");
 const successMsg = ref("");
 
-const signOut = () => {
-  getAuth().signOut();
-  router.push("/signin");
+const initiateSignOut = () => {
+  signOut(getAuth()).then(() => {
+    router.push("/signin");
+  });
 };
 
-const showUpdatePasswordModal = ref(false);
-const showUpdateEmailModal = ref(false);
+const showSignout = ref(false);
+const showUpdatePassword = ref(false);
+const showUpdateEmail = ref(false);
 
 const currentPassword = ref("");
 const newPassword = ref("");
@@ -169,3 +226,25 @@ const initiateEmailUpdate = async () => {
   }
 };
 </script>
+
+<style scoped>
+.app-disclosure-button {
+  @apply flex
+          justify-between
+          w-full
+          p-4
+          text-sm
+          font-medium
+          text-left text-blue-900
+          bg-blue-200
+          rounded-t
+          hover:bg-blue-300
+          focus:outline-none
+          focus-visible:ring
+          focus-visible:ring-blue-500;
+}
+
+.app-disclosure-panel {
+  @apply p-2 pt-3 text-sm text-gray-500 rounded-b border;
+}
+</style>
