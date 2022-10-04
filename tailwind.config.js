b9458c6f9dc402c3aade2e160d972fefcb65cfad
Andreas Rudolf <anrudolf@gmail.com>
2022-10-04 22:41:05 +0200

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,12 @@
 module.exports = {
-  purge: [
+  content: [
     "./index.html",
     "./src/**/*.html",
     "./src/**/*.vue",
     "./src/**/*.jsx",
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
-  },
-  variants: {
-    extend: {
-      opacity: ["disabled"],
-      ringColor: ["focus-visible"],
-      ringWidth: ["focus-visible"],
-      ringOffsetWidth: ["focus-visible"],
-      textDecoration: ["focus-visible"],
-      outline: ["focus-visible"],
-    },
   },
   plugins: [],
 };
