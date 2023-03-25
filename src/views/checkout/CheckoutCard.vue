a4a9876d43dedd26ca8c315a92999bfe9eb2ca4d
Andreas Rudolf <anrudolf@gmail.com>
2023-03-25 14:07:11 +0100

    checkout: implement card payment

--- src/views/checkout/CheckoutCard.vue
+++ src/views/checkout/CheckoutCard.vue
@@ -1,20 +1,40 @@
 <template>
   <app-button-back class="ml-2">Zurück</app-button-back>
 
-  <div class="p-4">
-    <h1>Bezahlen</h1>
-    <h2>Karte</h2>
+  <div class="mx-4" style="width: 300px">
+    <div class="mt-4 flex items-center justify-between">
+      <h1 class="text-2xl">Bezahlen</h1>
+      <span class="text-2xl text-gray-800"
+        >{{ kasse.price.toFixed(2) }} CHF</span
+      >
+    </div>
+    <h2 class="text-gray-500">Karte</h2>
+    <credit-card-icon class="w-48 h-48 text-blue-500"></credit-card-icon>
   </div>
 </template>
 
 <script lang="ts" setup>
+import useKasse from "@/store/kasse";
+import { CreditCardIcon } from "@heroicons/vue/24/outline";
+
 import appButtonBack from "@/components/ui/ButtonBack.vue";
 
 import useScanner from "../../hooks/use-scanner";
+import useCheckout from "@/hooks/use-checkout";
+
+const kasse = useKasse();
+const checkout = useCheckout();
 
 useScanner(
   (code) => {
-    console.log(`paid with card ${code}`);
+    console.log(`paying with card: ${code}`);
+    checkout({
+      success: true,
+      paymentMethod: "card",
+      price: kasse.price,
+      paid: kasse.price,
+      change: 0,
+    });
   },
   [10]
 );
