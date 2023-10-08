f3ac60e2ba3a497b53ea534adf87762861c23a80
Andreas Rudolf <anrudolf@gmail.com>
2023-10-08 09:21:10 +0200

    add back button to product edit page

--- src/views/products/ProductEdit.vue
+++ src/views/products/ProductEdit.vue
@@ -1,10 +1,12 @@
 <template>
+  <app-button-back class="ml-4 mb-3">Zurück</app-button-back>
   <app-product-edit :id="props.id" :key="`${editing}`" :editing="editing" />
 </template>
 
 <script lang="ts" setup>
 import { computed, defineProps } from "vue";
 import { useRoute } from "vue-router";
+import appButtonBack from "@/components/ui/ButtonBack.vue";
 
 import appProductEdit from "@/components/product/ProductEdit.vue";
 
